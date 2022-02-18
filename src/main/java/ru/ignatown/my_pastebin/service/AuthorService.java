bd559f7e90f59e455f008b1e331b42ff3d682d76
ignatown <ignatow.n@mail.ru>
2022-02-18 22:56:19 +0300

    add logback

--- src/main/java/ru/ignatown/my_pastebin/service/AuthorService.java
+++ src/main/java/ru/ignatown/my_pastebin/service/AuthorService.java
@@ -1,14 +1,16 @@
 package ru.ignatown.my_pastebin.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import ru.ignatown.my_pastebin.entity.Author;
 import ru.ignatown.my_pastebin.repository.AuthorRepository;
 
-import java.util.List;
-
 @Service
 public class AuthorService {
     private final AuthorRepository authorRepository;
+    private static final Logger logger = LoggerFactory.getLogger(
+            AuthorService.class);
 
     public AuthorService(AuthorRepository authorRepository) {
         this.authorRepository = authorRepository;
@@ -16,10 +18,12 @@ public class AuthorService {
 
     public Author save(Author author) {
         authorRepository.save(author);
+        logger.info("IN AuthorService save new author: " + author.getUsername());
         return author;
     }
 
     public Author findByUsername(String username) {
+        logger.info("IN findByUsername find author by username: " + username);
         return authorRepository.findAuthorByUsername(username);
     }
 
--- src/main/java/ru/ignatown/my_pastebin/service/PasteService.java
+++ src/main/java/ru/ignatown/my_pastebin/service/PasteService.java
@@ -1,5 +1,7 @@
 package ru.ignatown.my_pastebin.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import ru.ignatown.my_pastebin.entity.Paste;
@@ -15,6 +17,8 @@ import java.util.stream.Collectors;
 @Service
 public class PasteService {
     private final PasteRepository pasteRepository;
+    private static final Logger logger = LoggerFactory.getLogger(
+            PasteService.class);
 
     public PasteService(PasteRepository pasteRepository) {
         this.pasteRepository = pasteRepository;
@@ -29,15 +33,20 @@ public class PasteService {
         paste.setShortUrl(RandomStringUtils.randomAlphabetic(5));
         paste.setAuthorName(SecurityContextHolder.getContext().getAuthentication().getName());
         pasteRepository.save(paste);
+        logger.info("IN savePaste save new paste by pasteDTO: " + pasteDTO.getText()
+                + " with expiration time:" + pasteDTO.getExpirationTime());
         return paste;
     }
 
     public Paste getPaste(String shorURL) throws IllegalArgumentException {
+        logger.info("IN getPaste get paste by shortURL: " + shorURL);
         Paste findPaste = pasteRepository.findByShortUrl(shorURL);
         if (findPaste == null) {
+            logger.warn("IN getPaste not exist paste with shortURL: " + shorURL);
             throw new IllegalArgumentException("Not found paste with short url: " + shorURL);
         }
         if (!findPaste.getExpirationTime().isAfter(LocalDateTime.now())) {
+            logger.info("IN getPaste expiration time shortURL: " + shorURL);
             throw new IllegalArgumentException("The storage time of the paste with short url: " + shorURL + "has ended");
         }
         return findPaste;
